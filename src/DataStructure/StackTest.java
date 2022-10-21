59843a0b71c982cab0e2bc5775a498237ca56d61
nova1751 <nova2319427728@outlook.com>
2022-10-21 12:29:34 +0800

    update

---
+++ src/DataStructure/StackTest.java
@@ -0,0 +1,34 @@
+package DataStructure;
+
+import java.util.*;
+
+public class StackTest {
+    static void showPush(Stack<Integer> st, int a) {
+        st.push(new Integer(a));
+        System.out.println("push(" + a + ")");
+        System.out.println("stack:" + st);
+    }
+
+    static void showPop(Stack<Integer> st) {
+        System.out.print("pop -> ");
+        Integer a = (Integer) st.pop();
+        System.out.println(a);
+        System.out.println("stack:" + st);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>();
+        System.out.println("stack:" + st);
+        showPush(st, 42);
+        showPush(st, 66);
+        showPush(st, 99);
+        showPop(st);
+        showPop(st);
+        showPop(st);
+        try {
+            showPop(st);
+        } catch (EmptyStackException e) {
+            System.out.println("empty stack");
+        }
+    }
+}
