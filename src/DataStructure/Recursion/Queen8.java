f10575c278be91a011e9915c0b8aa7d48761a9a7
nova1751 <nova2319427728@outlook.com>
2022-11-03 18:35:10 +0800

    更新

--- src/DataStructure/Recursion/Queen8.java
+++ src/DataStructure/Recursion/Queen8.java
@@ -27,7 +27,8 @@ public class Queen8 {
 
     public static void main(String[] args) {
         Queen8 queen8 = new Queen8();
-        queen8.check(0); // 从第一行开始放置
+        // queen8.check(0); // 从第一行开始放置
+        queen8.judgeTest();
     }
 
     /**
@@ -52,7 +53,7 @@ public class Queen8 {
             if (judge(n)) {
                 // 如果不冲突表示放置的没问题
                 // 进入下一行皇后的放置
-                check(n + 1);
+                check(n + 1); 
             }
             // 如果冲突，这里什么也不做
             // 因为是从第一列开始放置，如果冲突，则尝试放置到第 2 列上，直到放置成功
@@ -65,32 +66,103 @@ public class Queen8 {
      * @param n 第 n 个皇后
      * @return
      */
-    private boolean judge(int n){
-        for(int i=0;i<n;i++){
-            if(
-                
-                    /*
-                     如果他们的摆放位置一样，说明是在同一列
-                      x ....
-                      x ....
-                     */
-                    array[i]==array[n]
-                    /*
-                              检测是否是同一斜列
-                              下标: 代表的是第几行
-                              值：代表的是第几列
-                              . x . . .  n = 1,value = 1
-                              x . . . .  i = 0,value = 0
-                              Math.abs(n - i) = 1
-                              Math.abs(array[n] - array[i]) = 1
+    private boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+            if (
 
-                              . . x . .  n = 1,value = 2
-                              . x . . .  i = 0,value = 1
-                              Math.abs(n - i) = 1
-                              Math.abs(array[n] - array[i]) = 1
-                             */
-                    || 
-            )
+            /*
+             * 如果他们的摆放位置一样，说明是在同一列
+             * x ....
+             * x ....
+             */
+            array[i] == array[n]
+                    /*
+                     * 检测是否是同一斜列
+                     * 下标: 代表的是第几行
+                     * 值：代表的是第几列
+                     * . x . . . n = 1,value = 1
+                     * x . . . . i = 0,value = 0
+                     * Math.abs(n - i) = 1
+                     * Math.abs(array[n] - array[i]) = 1
+                     * 
+                     * . . x . . n = 1,value = 2
+                     * . x . . . i = 0,value = 1
+                     * Math.abs(n - i) = 1
+                     * Math.abs(array[n] - array[i]) = 1
+                     */
+                    || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
         }
+        return true;
+    }
+
+    /**
+     * 打印皇后的位置
+     */
+    private void print() {
+        System.out.printf("第 %02d 个结果：", count);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 下面是对于判定是否是同行，同列、同一斜列的分步骤测试，比较好理解
+     */
+    public void judgeTest() {
+        /*
+         * . . . . . . . .
+         * x . . . . . . .
+         */
+        Queen8 queue8 = new Queen8();
+        queue8.array[0] = 0;
+
+        // ======== 放置第 1 个皇后
+        // 判断是否是同一列
+        /*
+         * x . . . . . . . // 计划放到这里
+         * x . . . . . . .
+         */
+        queue8.array[1] = 0; // 从第一列开始放置
+        System.out.println("同一列，是否通过：" + queue8.judge(1));
+
+        /*
+         * . x . . . . . . // 计划放到这里
+         * x . . . . . . .
+         */
+        queue8.array[1] = 1;
+        // 第一列不行，放置到第 2 列上
+        System.out.println("同一斜列，是否通过：" + queue8.judge(1));
+
+        /*
+         * . . x . . . . . // 计划放到这里
+         * x . . . . . . .
+         */
+        queue8.array[1] = 2;
+        // 第 2 列不行，放置到第 3 列上,这个肯定是可以的
+        System.out.println("同一列或同一斜列，是否通过：" + queue8.judge(1));
+
+        // ======== 放置第 3 个皇后
+        /*
+         * x . . . . . . . // 计划放到这里
+         * . . x . . . . .
+         * x . . . . . . .
+         */
+        queue8.array[2] = 0;
+        // 与第一行的在同一列上了
+        System.out.println("同一列，是否通过：" + queue8.judge(2));
+
+        /*
+         * . x . . . . . . // 计划放到这里
+         * . . x . . . . .
+         * x . . . . . . .
+         */
+        queue8.array[2] = 1;
+        // 第一列不行，放置到第 2 列
+        // 这里与第 2 行的同一斜列了，也是不行的
+        System.out.println("同一斜列，是否通过：" + queue8.judge(2));
+
     }
 }
